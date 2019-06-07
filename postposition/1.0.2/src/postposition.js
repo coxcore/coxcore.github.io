3398f46a266ded5635631cf70131a034b55ee756
coxcore <ascript0@gmail.com>
2019-06-07 14:16:08 +0900

    postposition 라이브러리 버전 오류 수정

--- postposition/1.0.2/src/postposition.js
+++ postposition/1.0.2/src/postposition.js
@@ -27,7 +27,15 @@ const KO_FINISH_CODE = 55203;
  * @private
  * @type {RegExp}
  */
-const REG_INVALID_CHAR = /[^a-zA-Z0-9ㄱ-ㅎ가-힣]*/g;
+const REG_INVALID_CHAR = /[^a-zA-Z0-9ㄱ-ㅎ가-힣\s]+/g;
+
+/**
+ * 종성을 체크할 글자 정규식
+ *
+ * @private
+ * @type {RegExp}
+ */
+const REG_TARGET_CHAR = /^[\s\S]*?(\S*)\s*$/;
 
 /**
  * 종성이 없는 조건 정규식
@@ -35,8 +43,8 @@ const REG_INVALID_CHAR = /[^a-zA-Z0-9ㄱ-ㅎ가-힣]*/g;
  * @private
  * @type {RegExp}
  */
-const REG_FIXED_NORMAL = new RegExp(`(?:${[
-    "check|[hm]ook",
+const REG_NORMAL_FIXED = new RegExp(`(?:${[
+    "check|[hm]ook|limit",
 ].join("|")})$`, "i");
 
 /**
@@ -46,16 +54,16 @@ const REG_FIXED_NORMAL = new RegExp(`(?:${[
  * @type {RegExp}
  */
 const REG_SPECIAL_CHAR = new RegExp(`(?:${[
-    "[ㄱ-ㅎ]",
-    "[013678]",
-    "^[lmnr]",
-    "\\S+[lmn]e?",
-    "(?:[aeiom]|\\Slu)b",
-    "(?:[aeiu]|[^o]o)p",
-    "(?:[iu]|[^e][ae]|[^o]o)t",
-    "(?:[iou]|[^e][ae])c?k",
-    "[aeiou](?:c|ng)",
-    "app|foot|go+d|big|bag",
+    "[ㄱ-ㄷㅁ-ㅎ036]",
+    "^[mn]",
+    "\\S[mn]e?",
+    "\\S(?:[aeiom]|lu)b",
+    "(?:u|\\S[aei]|[^o]o)p",
+    "(?:^i|[^auh]i|\\Su|[^ei][ae]|[^oi]o)t",
+    "(?:\\S[iou]|[^e][ae])c?k",
+    "\\S[aeiou](?:c|ng)",
+    "foot|go+d|b[ai]g|private",
+    "^(?:app|kor)",
 ].join("|")})$`, "i");
 
 /**
@@ -67,8 +75,17 @@ const REG_SPECIAL_CHAR = new RegExp(`(?:${[
 const REG_SPECIAL_RO = new RegExp(`(?:${[
     "[178ㄹ]",
     "^[lr]",
-    "\\S+le?",
+    "^\\Sr",
+    "\\Sle?",
 ].join("|")})$`, "i");
+
+/**
+ * 조사를 처리할 단어를 찾는 정규식
+ *
+ * @private
+ * @type {RegExp}
+ */
+const REG_PARSER_PATTERN = /(\S*)\[([\w가-힣]*)(?:\*|\|([\w가-힣]*))\]/g;
 
 /**
  * 기본적으로 지원하는 조사
@@ -102,83 +119,100 @@ const SPECIAL_POSTPOSITION = (() => {
     return special;
 })();
 
-
-/**
- * 조사가 '로'면 종성이 리을이 아닌 조건
- *
- * @private
- * @param type {string} 조사
- * @param state {boolean} 종성이 있는지 여부
- * @return {boolean}
- */
-const checkRo = (type, state) => (type !== "로" && type !== "으로") || !state;
-
 /**
  * 종성이 있는 문자열 체크
  *
  * @private
  * @param text {string} 종성이 있는지를 확인할 문자열
- * @param type {string} 조사
+ * @param isRo {boolean} 조사가 '로/으로'인지 여부
  * @return {boolean}
  */
-const checkText = (text, type) =>
-    // 지정한 종성이 없는 글자가 아닌 경우
-    !REG_FIXED_NORMAL.test(text) &&
-    // 종성이 있을 때
-    REG_SPECIAL_CHAR.test(text) &&
-    // 조사가 '로'인 경우 구분
-    checkRo(type, REG_SPECIAL_RO.test(text));
+const checkText = (text, isRo) => {
+    if (REG_NORMAL_FIXED.test(text)) {
+        return false;
+    }
+
+    // 한글 외에 'ㄹ' 종성 판단은 종성 유무 조건과 분리하여 개별 처리
+    return REG_SPECIAL_CHAR.test(text) || (!isRo && REG_SPECIAL_RO.test(text));
+};
 
 /**
  * 종성이 있는 한글의 유니 코드 체크
  *
  * @private
  * @param code {number} 유니 코드
- * @param type {string} 조사('로/으로'의 경우가 아니면 type 파라미터를 생략)
+ * @param isRo {boolean} 조사가 '로/으로'인지 여부
  * @returns {boolean}
  */
-const checkCode = (code, type) => {
+const checkCode = (code, isRo) => {
     const finalConsonantCode = (code - KO_START_CODE) % 28;
 
-    // 조사가 '로'면 종성이 리을이 아닐 때
-    return finalConsonantCode !== 0 && checkRo(type, finalConsonantCode === 8);
+    // 종성이 있는 경우를 체크하고, 조사 '로'일 때 종성이 'ㄹ'인지 여부 추가 체크
+    return finalConsonantCode !== 0 && !(isRo && finalConsonantCode === 8);
 };
 
+/**
+ * 문자열로 사용 가능한지 여부 체크
+ *
+ * @private
+ * @param text {string}
+ * @returns {boolean}
+ */
+const invalidText = text => {
+    const type = typeof text;
+
+    return type !== 'string' && type !== 'number';
+};
+
+/**
+ * 문장에서 찾은 패턴을 처리하는 callback function
+ *
+ * @private
+ * @param target 패턴과 일치하는 문자열
+ * @param word 대상 단어
+ * @param normal 종성이 없을 때 조사
+ * @param special 종성이 있을 때 조사
+ */
+const callbackParser = (target, word, normal, special) => put(word, normal, special);
 
 /**
  * 종성이 있는 문자열인지 여부
  * '로/으로'의 경우가 아니면 type 파라미터를 생략해도 된다.
  *
- * @param text 체크할 문자열
- * @param type 조사('로/으로'의 경우가 아니면 type 파라미터를 생략)
+ * @param text {string} 체크할 문자열
+ * @param type {string|null} 조사('로/으로'의 경우가 아니면 type 파라미터를 생략)
  * @returns {boolean}
  */
-export const check = (text, type) => {
-    const target = text.replace(REG_INVALID_CHAR, '');
-    const code = target.charAt(target.length - 1).charCodeAt();
-    const korean = KO_START_CODE <= code && code <= KO_FINISH_CODE;
+export const check = (text, type = null) => {
+    if (invalidText(text)) {
+        return false;
+    }
 
-    // 한글이면 종성이 있는 코드인지를 체크
-    return korean ? checkCode(code, type) : checkText(target, type);
+    const target = String(text).replace(REG_INVALID_CHAR, " ").replace(REG_TARGET_CHAR, "$1");
+    const code = target.charAt(target.length - 1).charCodeAt();
+    const isKorean = KO_START_CODE <= code && code <= KO_FINISH_CODE;
+    const isRo = type === "로" || type === "으로";
+
+    return isKorean ? checkCode(code, isRo) : checkText(target, isRo);
 };
 
 /**
  * 글자에 해당하는 조사를 반환
  * 아래 조사에 대해서는 special 파라미터를 전달하지 않아도 된다.
- * 은/는, 이/가, 을/를, 과/와, 로/으로
+ * 은/는, 이/가, 을/를, 과/와, 나/이나, 로/으로
  *
  * @param text {string} 조사를 붙일 문자열
  * @param type {string} 조사
- * @param special {string} 종성이 있을 때 조사
+ * @param special {string|null} 종성이 있을 때 조사
  * @returns {string}
  */
-export const pick = (text, type, special) => {
+export const pick = (text, type, special = null) => {
     if (typeof special !== "string") {
         type = DEFAULT_POSTPOSITION[type] || type || "";
         special = SPECIAL_POSTPOSITION[type] || type;
     }
 
-    return check(text, type) ? special : type;
+    return check(String(text), type) ? special : type;
 };
 
 /**
@@ -187,10 +221,10 @@ export const pick = (text, type, special) => {
  *
  * @param text {string} 조사를 붙일 문자열
  * @param type {string} 조사
- * @param special {string} 종성이 있을 때 조사
+ * @param special {string|null} 종성이 있을 때 조사
  * @return {string}
  */
-export const put = (text, type, special) => `${text}${pick(text, type, special)}`;
+export const put = (text, type, special = null) => `${text}${pick(text, type, special)}`;
 
 /**
  * 특정 조사를 처리하는 함수를 반환
@@ -198,11 +232,22 @@ export const put = (text, type, special) => `${text}${pick(text, type, special)}
  *
  * @param type {string} 조사
  * @param special {string} 종성이 없을 때 조사
+ * @return {function}
  */
-export const fix = (type, special) => function(text) {
-    return put(text, type, special);
-};
+export const fix = (type, special) => (text => put(text, type, special));
+
+/**
+ * 문자열에서 특정 패턴을 찾아 해당 문자에 맞는 조사를 처리
+ * 찾는 패턴 : 단어[ 종성이 없을 때 조사 | 종성이 있을 때 조사 ]
+ * ex) 문자열[를|을] 변경 => 문자열을 변경
+ *
+ * @param sentence 처리할 문자열
+ * @returns {string|*}
+ */
+export const parse = (sentence) => (typeof sentence === 'string') ?
+    sentence.replace(REG_PARSER_PATTERN, callbackParser) : sentence;
+
 
 export default {
-    check, pick, put, fix
+    check, pick, put, fix, parse
 }
